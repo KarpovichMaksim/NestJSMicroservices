a5d4793f8b74ede113608e604b229b7551698888
Maksim <the.butterfly.incorporated@gmail.com>
2022-01-25 22:21:31 +0300

    Change transport and add accumulate function

--- microservices/src/main.ts
+++ microservices/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const microservicesOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: '8877',
+  },
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microservicesOptions,
+  );
+  app.listen();
 }
 bootstrap();
